e3e8f9cc879fcc8b65c9a9a588682b173b32987d
unknown <TSD-AnhND48@TSD-AnhND48.fishcm.com.vn>
2019-07-31 10:42:48 +0700

    Behavioral Pattern: Command - Bank

---
+++ src/patterns/behavioral/command/bank/Client.java
@@ -0,0 +1,14 @@
+package patterns.behavioral.command.bank;
+
+public class Client {
+    public static void main(String[] args) {
+        Account account = new Account("FeedVN");
+
+        Command open = new OpenAccount(account);
+        Command close = new CloseAccount(account);
+        BankApp bankApp = new BankApp(open, close);
+
+        bankApp.clickOpenAccount();
+        bankApp.clickCloseAccount();
+    }
+}
