bfd285a65bca78305adfb975894c389b95edf288
unknown <TSD-AnhND48@TSD-AnhND48.fishcm.com.vn>
2019-08-05 16:40:02 +0700

    Structural: Bridge

---
+++ src/patterns/structural/bridge/CheckingAccount.java
@@ -0,0 +1,8 @@
+package patterns.structural.bridge;
+
+public class CheckingAccount implements Account {
+    @Override
+    public void openAccount() {
+        System.out.println("Checking Account");
+    }
+}
